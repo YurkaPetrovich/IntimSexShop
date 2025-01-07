63495b6162390ada983cb23097fec6ac63ecefea
IntimSexShop <yuriyparr@gmail.com>
2025-01-07 21:57:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Простий скрипт для додавання товару в кошик (симуляція)
+document.querySelectorAll('. add-to-cart').forEach(button => {
+    button.addEventListener(' click', () => {
+        alert('Товар додано до кошика');
+    });
+});
